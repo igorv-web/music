6a70e0dc8cbe843ebcd65d6ffcb1c35438b7cb1d
igorv-web <igorvilkov404@gmail.com>
2021-02-26 15:29:15 +0200

    HTTP get api

--- src/app/pages/genre/genre.component.ts
+++ src/app/pages/genre/genre.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/shared/service/api.service';
 
 @Component({
   selector: 'app-genre',
@@ -6,11 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./genre.component.scss']
 })
 export class GenreComponent implements OnInit {
+  albums: Object;
   isLiked: boolean = false;
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
+    this.getAlbums();
+  }
+
+  private getAlbums(): void {
+    this.apiService.getAlbum().subscribe(
+      (data) => {
+        this.albums = data;
+        console.log(data);
+      },
+      (err) => console.log(err)
+    )
   }
 
   addFavourite(): void {
--- src/app/shared/service/api.service.ts
+++ src/app/shared/service/api.service.ts
@@ -5,15 +5,14 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ApiService {
-  apiKey = '22e5dcb7293a23da484afeacce80c247';
 
   constructor(private http: HttpClient) { }
 
-  getMusic(genre: string) {
-    return this.http.get(`http://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&tag=${genre}&api_key=${this.apiKey}&format=json`);
+  getAlbum() {
+    return this.http.get(`http://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&tag=rock&api_key=22e5dcb7293a23da484afeacce80c247&format=json`);
   }
 
   searchMusic(search: string) {
-    return this.http.get(`http://ws.audioscrobbler.com/2.0/?method=album.search&album=${search}&api_key=${this.apiKey}&format=json`)
+    return this.http.get(`http://ws.audioscrobbler.com/2.0/?method=album.search&album=${search}&api_key=22e5dcb7293a23da484afeacce80c247&format=json`);
   }
 }
